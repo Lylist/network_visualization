aec6b754fbd3f715cee4f31665a4ecb93fb2e4c0
Lylist <1303404574@qq.com>
2019-09-06 00:39:04 +0800

    '1.0版本'

--- data_analyze/analysis.py
+++ data_analyze/analysis.py
@@ -2,124 +2,157 @@
 # coding=utf-8
 
 import queue
+from functools import cmp_to_key
+import math
+PI = math.pi
 
-class Analyzer:
+
+class Analyzer(object):
+    '''
+        point: 所有点
+        data: id与标记数组和逻辑的kv字典
+        vis: 标记kv度
+        r:钟表的半径
+        circle_center: 圆心
+        delta: 每次移动的角度偏量
+        alpha: 当前钟表指针与x轴的夹角
+        iniradius: 当前层，内环半径
+    '''
+
     def __init__(self, point):
         self.point = point
         # 用于存id与点的kv对
         self.data = {}
         self.vis = {}
 
+        self.iniradius = 5
+        self.r = 5
+        self.circle_center = {'x': 0.0, 'y': 0.0, 'z': 0.0}
+        self.delta = PI/2
+        self.alpha = 0
+
         self.dir = [[10, -10, 0, 0], [0, 0, 10, -10], [0, 0, 0, 0]]
 
-    def sort(self):
-        pass
+    def sort(self, sort_list):
+        '''
+        用于排序，按节点度数排序
+        :param sort_list: 传入要排序的list
+        :return: 排好序的list
+        '''
+        length = len(sort_list)
+        for i in range(length):
+            for j in range(length-i-1):
+                len1 = len(sort_list[j]['link'])
+                len2 = len(sort_list[j+1]['link'])
+                condition = lambda x: 1 if x < 5 else (2 if 5 <= x < 50 else 3)
+                layer1 = condition(len1)
+                layer2 = condition(len2)
+                if len1 > len2 and layer1 != layer2:
+                    k = sort_list[j]
+                    sort_list[j] = sort_list[j+1]
+                    sort_list[j+1] = k
 
-    def cal_center_point(self):
-        sum_x, sum_y, sum_z = 0, 0, 0
-        for k, v in self.vis.items():
-            tmp = k.split(':')
-            tmp_x = float(tmp[0])
-            tmp_y = float(tmp[1])
-            tmp_z = float(tmp[2])
+        # sorted(sort_list, key=cmp_to_key(lambda x, y: len(x['link']) < len(y['link'])))
+        return sort_list
 
-            sum_x = sum_x + tmp_x
-            sum_y = sum_y + tmp_y
-            sum_z = sum_z + tmp_z
+    def block_init_begin(self):
+        self.iniradius = 5
+        self.r = 5
+        self.circle_center = {'x': 0.0, 'y': 0.0, 'z': 0.0}
+        self.delta = PI / 2
+        self.alpha = 0
 
-        sum_x = sum_x / len(self.vis)
-        sum_y = sum_y / len(self.vis)
-        sum_z = sum_z / len(self.vis)
+    def block_init_fulllayer(self):
+        '''
+        当铺完一层后初始化r、delta、alpha和circlr_center
+        :return: 无返回值
+        '''
+        self.iniradius = (self.iniradius+self.r)/2
+        self.r = self.iniradius
+        self.delta = PI/2
+        self.alpha = 0
+        self.circle_center['x'] = 0.0
+        self.circle_center['z'] = 0.0
+        self.circle_center['y'] = self.circle_center['y']+40
 
-        return sum_x, sum_y, sum_z
+    def block_init_fullcircle(self):
+        '''
+        当排满一圈后初始化r、delta和alpha
+        :return: 无返回值
+        '''
+        self.r += self.iniradius
+        self.delta = (PI/2)*(self.iniradius/self.r)
+        self.alpha = 0
 
-    def balance(self):
-        avg_x, avg_y, avg_z = self.cal_center_point()
+    def get_block_location(self):
+        '''
+        获取钟表算法中指针的坐标，同时动态维护钟表算法的
+        :return: x,y,z值
+        '''
 
-        for i in range(len(self.point)):
-            p = self.point[i]
-            x = p['location']['x']
-            y = p['location']['y']
-            z = p['location']['z']
-            self.point[i]['location'].update(dict(x=x-avg_x, y=y-avg_y, z=z-avg_z))
+        # 当排满一圈后需要重新确定r、delta和alpha
+        if self.alpha >= PI * 2:
+            self.block_init_fullcircle()
 
-    def find_new_block(self):
-        x = 0
-        y = 0
-        z = 0
-        if self.vis == {}:
-            return x, y, z
-
-        avg_x, avg_y, avg_z = self.cal_center_point()
-        min_x = dict(x=100000000000, y=0, z=0)
-        min_y = dict(x=0, y=100000000000, z=0)
-        min_z = dict(x=0, y=0, z=100000000000)
-        max_x = dict(x=-100000000000, y=0, z=0)
-        max_y = dict(x=0, y=-100000000000, z=0)
-        max_z = dict(x=0, y=0, z=-100000000000)
-
-        for k, v in self.vis.items():
-            tmp = k.split(':')
-            tmp_x = float(tmp[0])
-            tmp_y = float(tmp[1])
-            tmp_z = float(tmp[2])
-            if tmp_x > max_x['x']:
-                max_x.update(dict(x=tmp_x, y=tmp_y, z=tmp_z))
-            if tmp_y > max_y['y']:
-                max_y.update(dict(x=tmp_x, y=tmp_y, z=tmp_z))
-            if tmp_z > max_z['z']:
-                max_z.update(dict(x=tmp_x, y=tmp_y, z=tmp_z))
-
-            if tmp_x < min_x['x']:
-                min_x.update(dict(x=tmp_x, y=tmp_y, z=tmp_z))
-            if tmp_y < min_y['y']:
-                min_y.update(dict(x=tmp_x, y=tmp_y, z=tmp_z))
-            if tmp_z < min_z['z']:
-                min_z.update(dict(x=tmp_x, y=tmp_y, z=tmp_z))
-
-        # 找距离最近的，尽量让中心点居中
-        new_location = max_x
-        dis = (avg_x - max_x['x']) ** 2 + (avg_y - max_x['y']) ** 2 + (avg_z - max_x['z']) ** 2
-        for item in [max_x, max_y, max_z, min_x, min_y, min_z]:
-            dis2 = (avg_x - item['x']) ** 2 + (avg_y - item['y']) ** 2 + (avg_z - item['z']) ** 2
-            if dis2 < dis:
-                new_location = item
-                dis =dis2
-
-        x = new_location['x']
-        y = new_location['y']
-        z = new_location['z']
-        for i in range(4):
-            nxt_x = x + self.dir[0][i] * 3
-            nxt_y = y + self.dir[1][i] * 3
-            nxt_z = z
-            pos = str(nxt_x) + ':' + str(nxt_y) + ':' + str(nxt_z) + ':'
-            if self.vis.get(pos, False):
-                continue
-            x, y, z = nxt_x, nxt_y, nxt_z
-            break
-
+        x = self.r*math.cos(self.alpha)
+        z = self.r*math.sin(self.alpha)
+        y = self.circle_center['y']
+        self.alpha += self.delta
         return x, y, z
 
-    def genearte_location(self):
-        bfs_queue = queue.Queue()
+    def location_plan(self, network):
+        '''
+        给一个网络按照钟表算法安排位置
+        :param network: 一个是联通网络所有point的list
+        :return: 无返回值，每个point的x,y,z坐标改变
+        '''
 
+        self.sort(network)
+
+        self.r = 10
+        self.circle_center = {'x': 0.0, 'y': 0.0, 'z': 0.0}
+        self.delta = PI / 2
+        self.alpha = 0
+        layer2 = False
+        layer3 = False
+        for i, point in enumerate(network):
+            index = self.data[point['id']]['index']
+            length = len(point['link'])
+            if length < 5:
+                x, y, z = self.get_block_location()
+                self.point[index]['location'].update(dict(x=x, y=y, z=z))
+            if 5 <= length < 50:
+                if not layer2:
+                    layer2 = True
+                    self.block_init_fulllayer()
+
+                x, y, z = self.get_block_location()
+                self.point[index]['location'].update(dict(x=x, y=y, z=z))
+            if length >= 50:
+                if not layer3:
+                    layer3 = True
+                    self.block_init_fulllayer()
+
+                x, y, z = self.get_block_location()
+                self.point[index]['location'].update(dict(x=x, y=y, z=z))
+
+    def balance(self):
+        pass
+
+    def generate_location(self):
+        bfs_queue = queue.Queue()
+        network = []
         for i, p in enumerate(self.point):
             if self.data[p['id']].get('vis', False):
                 continue
 
             bfs_queue.put(p)
-            x, y, z = self.find_new_block()
-            self.point[p['id']].update({'location': dict(x=x, y=y, z=z)})
-            pos = str(p['location']['x']) + ':' + str(p['location']['y']) + ':' + str(p['location']['z'])
-            self.vis[pos] = True
             self.data[p['id']]['vis'] = True
+            network.append(p)
 
             while(bfs_queue.empty() != True):
                 p = bfs_queue.get()
-                x = p['location']['x']
-                y = p['location']['y']
-                z = p['location']['z']
+
                 for nxt_id in p['link']:
                     if self.data[nxt_id].get('vis', False):
                         continue
@@ -127,27 +160,23 @@ class Analyzer:
                     nxt_p = self.point[self.data[nxt_id]['index']]
                     bfs_queue.put(nxt_p)
                     self.data[nxt_id]['vis'] = True
-                    nxt_z = 0
-                    for i in range(4):
-                        nxt_x = x + self.dir[0][i]
-                        nxt_y = y + self.dir[1][i]
-                        pos = str(nxt_x) + ':' + str(nxt_y) + ':' + str(nxt_z) + ':'
-                        if self.vis.get(pos, False):
-                            continue
-                        self.point[nxt_id]['location'].update(dict(x=nxt_x, y=nxt_y, z=nxt_z))
-                        self.vis[pos] = True
-                        break
+                    network.append(nxt_p)
 
+        network = self.sort(network)
+        self.location_plan(network)
+        # for p in network:
+        #     print("%s: %s" % (p['id'], str(p['link'])))
 
     def process(self):
-        self.sort()
 
         for i, p in enumerate(self.point):
+            p['link'] = list(p['link'])
             self.data[p.get('id')] = dict(index=i)
 
-        self.genearte_location()
+        self.generate_location()
         self.balance()
         return self.point
+
 
 if __name__ == '__main__':
     analyzer = Analyzer([])
--- data_process/processor.py
+++ data_process/processor.py
@@ -1,125 +1,180 @@
 #!/usr/bin/python
 # coding=utf-8
 import sys
-sys.path.append('../')
-
-import csv
 import json
+import math
 from data_analyze.analysis import Analyzer
 
+sys.path.append('../')
 
-# TODO 获取位置
-def getLocation():
-    return 1, 1, 1
 
-# TODO 获取设备
-def getDevice():
-    return 'computer'
+class DataProcessor(object):
+    '''
+        res: 所有节点的list
+        send： 所有发送数据的list, 按时间轴存，共两个小时，每个item存0.5s的发文
+        save_path: 数据存储的位置
+        input_file: 源数据
+        file_type: 数据类型，csv或者pcap
+        ip_set：所有ip的集合
+        max_time: 数据最大的时间戳
+    '''
 
-def _save_res(res):
-    json_str = json.dumps({'point_data': res})
-    with open('../data/res.json', 'w') as wf:
-        wf.write(json_str)
-    wf.close()
+    def __init__(self, input_file='../data/data.csv', file_type='csv', save_path='../data'):
+        self.res = []
+        self.send = [[] for i in range(14405)]
+        self.save_path = save_path
+        self.input_file = input_file
+        self.file_type = file_type
+        self.ip_set = {}
+        self.max_time = -1.0
+        self.all_time = 144
 
-def _save_send(send):
-    json_str = json.dumps({'send_data': send})
-    with open('../data/send.json', 'w') as wf:
-        wf.write(json_str)
-    wf.close()
+    def _save_res(self):
+        json_str = json.dumps({'point_data': self.res})
+        with open(self.save_path + '/res.json', 'w') as wf:
+            wf.write(json_str)
+        wf.close()
 
-def process(input_file='../data/20180630_anomalous_suspicious.csv', file_type='csv'):
+    def _save_send(self):
+        json_str = json.dumps({'send_data': self.send})
+        with open(self.save_path + '/send.json', 'w') as wf:
+            wf.write(json_str)
+        wf.close()
 
-    reader = csv.reader(open(input_file))
-    ipIdDict = dict()
+    def remove_duplicates(self):
+        '''
+        去重，去除每个点link中的冗余，只留下从小到大的连接
+        :return:
+        '''
+        for p in self.res:
+            condition = lambda x: x < p['id']
+            p['link'] = list(filter(condition, p['link']))
 
-    res = []
-    st = ''
-    des = ''
-    send = []
-    message = []
-    for list in reader:
-        if list[1] == '' or list[3] == '':
-            continue
-        # 很trick，不要第一行
-        if list[0] == 'anomalyID':
-            continue
-        if not ipIdDict.__contains__(list[1]) and list[1] != '':
-            ipIdDict[list[1]] = len(ipIdDict)
-        if not ipIdDict.__contains__(list[3]) and list[3] != '':
-            ipIdDict[list[3]] = len(ipIdDict)
-        id1 = ipIdDict[list[1]]
-        id2 = ipIdDict[list[3]]
-        if len(res) >= id1:
-            pointDict = dict()
-            pointDict["id"] = id1
-            pointDict["ip"] = list[1]
-            x,y,z = getLocation()
-            loc = dict()
-            loc["x"] = x
-            loc["y"] = y
-            loc["z"] = z
-            pointDict["location"] = loc
-            pointDict["device"] = getDevice()
-            pointDict["link"] = [id2]
-            res.append(pointDict)
-        if len(res) >= id2:
-            pointDict = dict()
-            pointDict["id"] = id2
-            pointDict["ip"] = list[3]
-            x,y,z = getLocation()
-            loc = dict()
-            loc["x"] = x
-            loc["y"] = y
-            loc["z"] = z
-            pointDict["location"] = loc
-            pointDict["device"] = getDevice()
-            pointDict["link"] = [id2]
-            pointDict["link"] = [id1]
-            pointDict["device"] = getDevice()
-            res.append(pointDict)
+    def add_point(self, source_ip, des_ip):
+        '''
+        添加一个节点对的节点信息
+        :param source_ip: 源ip
+        :param des_ip: 目的ip
+        :return: 无
+        '''
+        if not self.ip_set.get(source_ip):
+            new_id = len(self.ip_set)
+            self.ip_set[source_ip] = new_id
+            self.res.append(dict(id=new_id, ip=source_ip, link=set([]), device='computer',
+                                 location=dict(x=0, y=0, z=0)))
+        if not self.ip_set.get(des_ip):
+            new_id = len(self.ip_set)
+            self.ip_set[des_ip] = new_id
+            self.res.append(dict(id=new_id, ip=des_ip, link=set([]), device='computer',
+                                 location=dict(x=0, y=0, z=0)))
+        des_id = self.ip_set[des_ip]
+        source_id = self.ip_set[source_ip]
+        self.res[des_id]['link'].add(source_id)
+        self.res[source_id]['link'].add(des_id)
 
-        if len(res[id1]["link"]) == 0 or res[id1]["link"].count(id2) == 0:
-            res[id1]["link"].append(id2)
-        if len(res[id2]["link"]) == 0 or res[id2]["link"].count(id1) == 0:
-            res[id2]["link"].append(id1)
+    def add_message(self, timestamp, source_ip, source_port, des_ip, des_port, protocol):
+        '''
+        添加一条数据报
+        :param timestamp: 发报文时间
+        :param source_ip: 源ip
+        :param source_port: 源ip端口
+        :param des_ip: 目的ip
+        :param des_port: 目的ip端口
+        :param protocol: 协议
+        :return: 无
+        '''
+        if self.max_time < 0.0:
+            raise Exception('ERROR: This data has wrong time data! --- max_time has wrong %f' %  self.max_time)
+        try:
+            index = math.ceil(self.all_time * float(timestamp) / self.max_time)
+        except Exception as e:
+            raise Exception('ERROR: This data has wrong time data! --- timestamp has wrong %s' % timestamp)
 
-        if st == '' or des == '':
-            st = list[1]
-            des = list[3]
-            messDict = dict()
-            messDict["taxonomy"] = list[5]
-            messDict["srPort"] = list[2]
-            messDict["dstPort"] = list[4]
-            messDict["length"] = 1
-            message.append(messDict)
-        elif st == list[1] and des == list[3]:
-            messDict = dict()
-            messDict["taxonomy"] = list[5]
-            messDict["srPort"] = list[2]
-            messDict["dstPort"] = list[4]
-            messDict["length"] = 1
-            message.append(messDict)
+        if not (0 <= index < 14405):
+            raise Exception('ERROR: This data has wrong time data! --- index has wrong')
+
+        self.send[index].append(dict(srcID=self.ip_set[source_ip],
+                                     desID=self.ip_set[des_ip],
+                                     timeLength=1,
+                                     sport=source_port,
+                                     dport=des_port,
+                                     protocol=protocol))
+
+    def process_csv(self):
+        '''
+        对csv文件的处理，csv文件一定需要按照time, source_ip, source_port, des_ip, des_port, protocol存下来
+        :return:
+        '''
+        # 寻找时间最大值
+        is_first_line = True
+        with open(self.input_file, 'rb') as f:
+            for xline in f:
+                if is_first_line:
+                    is_first_line = False
+                    continue
+                try:
+                    line = xline.decode('utf-8')
+                    data = line.split(',')
+                    real_time = data[0]
+
+                    if '"' in real_time or "'" in real_time:
+                        real_time = eval(real_time)
+
+                except Exception as e:
+                    continue
+            self.max_time = max(self.max_time, float(real_time))
+        f.close()
+
+        is_first_line = True
+        with open(self.input_file, 'rb') as f:
+            for xline in f:
+                if is_first_line:
+                    is_first_line = False
+                    continue
+                # try:
+                line = xline.decode('utf-8')
+                data = line.split(',')
+                real_time = data[0]
+                source_ip = data[1]
+                source_port = data[2]
+                des_ip = data[3]
+                des_port = data[4]
+                protocol = data[5]
+
+                if source_ip == '' or des_ip == '':
+                    continue
+                # 处理嵌套字符串的情况
+                if '"' in real_time or "'" in real_time:
+                    real_time = eval(real_time)
+                self.add_point(source_ip, des_ip)
+                self.add_message(real_time, source_ip, source_port, des_ip, des_port, protocol)
+
+                # except Exception as e:
+                #     print(str(e))
+        f.close()
+
+    def process_pcap(self):
+        '''
+        处理pcap包
+        :return:
+        '''
+        pass
+
+    def process(self):
+        if self.file_type == 'csv':
+            self.process_csv()
         else:
-            sendDict = dict()
-            sendDict["srcID"] = ipIdDict[st]
-            sendDict["desID"] = ipIdDict[des]
-            messDict = dict()
-            messDict["taxonomy"] = list[5]
-            messDict["srPort"] = list[2]
-            messDict["dstPort"] = list[4]
-            messDict["length"] = 1
-            message.append(messDict)
-            sendDict["message"] = message
-            send.append(sendDict)
-            message = []
-            st = list[1]
-            des = list[3]
+            self.process_pcap()
 
-    analyzer = Analyzer(res)
-    res = analyzer.process()
-    _save_res(res)
-    _save_send(send)
+        analyzer = Analyzer(self.res)
+        self.res = analyzer.process()
+
+        # for p in self.res:
+        #     print("%s: %s" % (p['id'], str(p['link'])))
+        # self.remove_duplicates()
+        self._save_res()
+        self._save_send()
+        print('sucess')
 
 
 if __name__ == '__main__':
